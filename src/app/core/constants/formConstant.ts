95c3fd523222b6aa9f528b23d3ad671ee73bfbfc
Vinod-V3 <vinod@tunerlabs.com>
2023-02-25 13:29:15 +0530

    form constant changes

--- src/app/core/constants/formConstant.ts
+++ src/app/core/constants/formConstant.ts
@@ -2,8 +2,8 @@ import { IFORM } from '../interface/form';
 
 export const EDIT_PROFILE_FORM: IFORM = {
 	"type": "profile",
-	"subType": "profileForm",
-	"action": "profileFields",
+	"subType": "createProfile",
+	"action": "formFields",
 	"ver": "1.0",
 	"templateName": "defaultTemplate",
   };
